9ab129ae8f34355211b98a18d9583b978bb86a82
hariharan p <87015778+HHPee@users.noreply.github.com>
2022-05-11 21:55:32 +0530

    Delete extention.py

--- extention.py
+++
@@ -1,8 +0,0 @@
-'''
-NAME:HARIHARAN P
-DATE:09/05/2022
-'''
-
-nameoffile = input("Input the Filename: ")
-extention = nameoffile.split(".")
-print ("\n The extension of the file is : " + repr(extention[-1]))
