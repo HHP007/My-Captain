56811609c8bc7cfb1205286eb0e1849fd62fd6e5
hariharan p <87015778+HHPee@users.noreply.github.com>
2022-05-11 21:55:44 +0530

    Add files via upload

---
+++ extention.py
@@ -0,0 +1,8 @@
+'''
+NAME:HARIHARAN P
+DATE:09/05/2022
+'''
+
+nameoffile = input("Input the Filename: ")
+extention = nameoffile.split(".")
+print ("\n The extension of the file is : " + extention[-1])
